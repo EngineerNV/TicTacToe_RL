b521a8c2797442b5982898326ad676d9560bb650
EngineerNV <nvaughn100@gmail.com>
2019-12-25 22:29:51 -0800

    Added updates to some of q modules and ran a test confirming it works and updaes q table

--- qLearning.py
+++ qLearning.py
@@ -16,35 +16,55 @@
 
 from ticTac import ticTac
 
-class qLearning:
-	def __init__(self, alpha, gamma):
-		self.qTable = {} # this will be a dictionary where the state is the key, and the list is 
-		self.learrningRate = alpha
-		self.discountRate = gamma
-		
-	def updateQ(self, state, action, value): # this updates the q table, and intializes the Q row if not already  
-		if state in self.qTable:
-			qActList = self.qTable[state]
-			qActList[action] = value
-		else:
-			qActList = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-			qActList[action] = value
-			self.qTable[state] = qActList
 
-	def getQ(self, state, action=None):
-		if state not in self.qTable:# initialize q table state action set if not already in dictionary
-			self.qTable[state] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-		if action == None:# return entire action list if no action specified
-			return self.qTable[state]	
-		qActList = self.qTable[state]
-		return qActList[action]
-		
-	def learn(self, state, nextState, action, ticTac, player): # Q(s,a) = (1-alpha)*Q(s,a) + alpha*(R_t+gamma* Q_a max(s_t+1,a))
-		futureQList = []
-		reward = ticTac.moveReward(player)
-		qActions = self.getQ(nextState)
-		for i in ticTac.emptySpaces():
-			futureQList.append(qActions[i])
-		learningChange = reward + self.discountRate*( max( futureQList ) )
-		newQValue = self.getQ(state,action)*(1-self.learningRate) + self.learningRate*learningChange
-		self.updateQ(state, action, newQValue)
+class qLearning:
+    def __init__(self, alpha, gamma):
+        self.qTable = {}  # this will be a dictionary where the state is the key, and the list is
+        self.learningRate = alpha
+        self.discountRate = gamma
+
+    def updateQ(self, state, action, value):  # this updates the q table, and intializes the Q row if not already
+        if state in self.qTable:
+            qActList = self.qTable[state]
+            qActList[action] = value
+        else:
+            qActList = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+            qActList[action] = value
+            self.qTable[state] = qActList
+
+    def getQ(self, state, action=None):
+        if state not in self.qTable:  # initialize q table state action set if not already in dictionary
+            self.qTable[state] = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+        if action == None:  # return entire action list if no action specified
+            return self.qTable[state]
+        qActList = self.qTable[state]
+        return qActList[action]
+
+    def learn(self, state, nextState, action, ticTac,
+              player):  # Q(s,a) = (1-alpha)*Q(s,a) + alpha*(R_t+gamma* Q_a max(s_t+1,a))
+        futureQList = []
+        reward = ticTac.moveReward(player)
+        qActions = self.getQ(nextState)
+        for i in ticTac.emptySpaces():
+            futureQList.append(qActions[i])
+        learningChange = reward + self.discountRate * (max(futureQList))
+        newQValue = self.getQ(state, action) * (1 - self.learningRate) + self.learningRate * learningChange
+        self.updateQ(state, action, newQValue)
+
+q = qLearning(.5, .2)
+tic = ticTac()
+tic.playerTurn_linBoard(2, 1)
+state = tic.board2Key()
+tic.playerTurn_linBoard(1, 6)
+tic.playerTurn_linBoard(2, 2)
+nextState = tic.board2Key()
+q.learn(state, nextState, 6, tic, 1)
+
+state = tic.board2Key()
+tic.playerTurn_linBoard(1, 7)
+tic.playerTurn_linBoard(2, 0)
+nextState = tic.board2Key()
+
+
+q.learn(state, nextState, 7, tic, 1)
+print(q.qTable)
